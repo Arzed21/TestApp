20c27a5586719c42e07866c3ab1c105a984f8de9
Arzed21 <rasoolzahedi33@gmail.com>
2020-10-05 18:09:26 +0330

    first commit

---
+++ app/src/main/java/com/example/apptest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.apptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    Boolean isGhost = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = findViewById(R.id.videoView);
+        MediaController mediaControllerALI = new MediaController(this);
+        videoView.setMediaController(mediaControllerALI);
+
+
+        Uri uriAparat = Uri.parse("https://as7.cdn.asset.aparat.com/aparat-video/7e08f495e590203ec9b8225b6d27facd24550096-1080p.mp4");
+        videoView.setVideoURI(uriAparat);
+        videoView.start();
+
+
+
+    }
+
+
+
+}
